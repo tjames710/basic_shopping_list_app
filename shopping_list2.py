ccc7849212840193246ba06c3935e190a7e17d19
Ted <trunyonworks@gmail.com>
2017-05-29 16:35:41 -0400

    Added the ability to remove any item

--- shopping_list2.py
+++ shopping_list2.py
@@ -14,6 +14,7 @@ def show_help():
 Enter 'DONE' to stop adding items.
 Enter 'HELP' for this help.
 Enter 'SHOW' to see your current list.
+Enter 'REMOVE' to remove an item by name.
 """)
 
 def add_to_list(new_item):
@@ -21,10 +22,19 @@ def add_to_list(new_item):
     shopping_list.append(new_item)
     print("Added {}. List now has {} items.".format(new_item, len(shopping_list)))
 
+def remove_item():
+    item = input("What item would you like removed > ")
+    shopping_list.remove(item)
+    print("item {} has been removed. List now has {} items.".format(item, len(shopping_list)))
+    
+
+        
+
 def show_list():
     print("Here's your list: ")
     for item in shopping_list:
         print(item)
+
 
 
 def main():
@@ -43,9 +53,16 @@ def main():
         elif new_item == "SHOW":
             show_list()
             continue
+        elif new_item == "REMOVE":
+            remove_item()
+            continue
+            
+
+
         add_to_list(new_item)
 
 
     # print out the list
     show_list()
+
 main()
