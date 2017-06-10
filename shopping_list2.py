b868093656cc0a3330e7f613ed30320b758a5914
Ted <trunyonworks@gmail.com>
2017-06-09 22:33:37 -0400

    added an indexing functionality

--- shopping_list2.py
+++ shopping_list2.py
@@ -1,5 +1,4 @@
-# have a HELP command
-# have a SHOW command
+import os
 
 
 
@@ -7,7 +6,13 @@
 # make a list to hold onto our items
 shopping_list = []
 
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 def show_help():
+    clear_screen()
     # print out instructions on how to use the app
     print("What should we pick up at the store?")
     print("""
@@ -17,24 +22,44 @@ Enter 'SHOW' to see your current list.
 Enter 'REMOVE' to remove an item by name.
 """)
 
+
 def add_to_list(new_item):
-    # add new items to our list
-    shopping_list.append(new_item)
-    print("Added {}. List now has {} items.".format(new_item, len(shopping_list)))
+    show_list()
+    if len(shopping_list):
+        position = input("Where should I add {}?\n"
+                         "Press ENTER to add to the end of the list\n"
+                         "> ".format(new_item))
+    else:
+        position = 0
+
+    try:
+        position = abs(int(position))
+    except ValueError:
+        position = None
+    if position is not None:
+        shopping_list.insert(position-1, new_item)
+    else:
+        shopping_list.append(new_item)
+    
+    show_list()
+    
+
 
 def remove_item():
     item = input("What item would you like removed > ")
     shopping_list.remove(item)
     print("item {} has been removed. List now has {} items.".format(item, len(shopping_list)))
-    
 
-        
 
 def show_list():
+    clear_screen()
     print("Here's your list: ")
+    index = 1
     for item in shopping_list:
-        print(item)
+        print("{}. {}".format(index, item))
+        index += 1
 
+    print("-" * 10)
 
 
 def main():
@@ -45,23 +70,20 @@ def main():
         new_item = input("> ")
 
         # be able to quit the app
-        if new_item == "DONE":
+        if new_item.upper() == "DONE" or new_item.upper() == 'QUIT':
             break
-        elif new_item == "HELP":
+        elif new_item.upper() == "HELP":
             show_help()
             continue
-        elif new_item == "SHOW":
+        elif new_item.upper() == "SHOW":
             show_list()
             continue
-        elif new_item == "REMOVE":
+        elif new_item.upper() == "REMOVE":
             remove_item()
             continue
+        else:
+            add_to_list(new_item)
             
-
-
-        add_to_list(new_item)
-
-
     # print out the list
     show_list()
 
